f99ee1385d99425846018a0faa0ad9a4f1b511d4
DtrMika <mikail.dutra.93@gmail.com>
2023-10-10 10:27:25 -0300

    Actualizacion y mejora de carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,27 @@
-const lista = document.getElementById("carrito");
+const carrito = document.getElementById("carro");
 
+function showCarrito() {
+  // Obtiene los productos almacenados en localStorage
+  const carritoProductos =
+    JSON.parse(localStorage.getItem("carritoProductos")) || [];
 
-let productosDatal
+  // Muestra los productos en la tabla del carrito
+  carritoProductos.forEach((producto) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${producto.producto}</td>
+        <td>${producto.precio}</td>
+        <td>${producto.cantidad}</td>
+        <td>${producto.precio * producto.cantidad}</td>
+      `;
+    carrito.appendChild(row);
+  });
+}
+
+// Llama a la función para mostrar los productos en el carrito al cargar la página
+showCarrito();
+let productosDatal;
+
 function calcularSubtotal(precio, cantidad) {
-    return precio * cantidad;
+  return precio * cantidad;
 }
--- js/script.js
+++ js/script.js
@@ -1,28 +1,26 @@
 const lista = document.getElementById("lista");
 const submitBtn = document.getElementById("submit");
-const URL = "productos.json"
+const URL = "productos.json";
 const seleccionadoDiv = document.getElementById("seleccionado");
 const inputCantidad = document.getElementsByClassName("cantidad");
+const carro = document.getElementById("carro");
 
 let productosData;
 let carritoProducto = [];
 
 fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        showData(data);
-    })
-    .catch(err => console.log(err))
-
-
-
+  .then((response) => response.json())
+  .then((data) => {
+    productosData = data;
+    console.log(data);
+    showData(data);
+  })
+  .catch((err) => console.log(err));
 
 function showData(data) {
-    data.forEach(element => {
-        const row = document.createElement("tr");
-        row.innerHTML += ` 
-                <td>${element.id}</td>
+  data.forEach((element) => {
+    const row = document.createElement("tr");
+    row.innerHTML += `
                 <td>${element.producto}</td>
                 <td>${element.descripcion}</td>
                 <td>${element.precio}</td>
@@ -31,58 +29,65 @@ function showData(data) {
                     <button class="btn btn-primary btnAdd" id="btnAdd${element.id}">add</button>
                 </td>
             `;
-        lista.appendChild(row);
+    lista.appendChild(row);
 
-        const btnAdd = document.getElementById(`btnAdd${element.id}`);
-        btnAdd = document.getElementById(`"btnAdd${element.id}"`);
-        btnAdd.addEventListener("click", () => {
+    const btnAdd = document.getElementById(`btnAdd${element.id}`);
+    if (btnAdd) {
+      btnAdd.addEventListener("click", () => {
+        const cantidadIngresada = row.querySelector(".numero");
+        const cantidad = parseInt(cantidadIngresada.value);
+        const productosGuardados =
+          JSON.parse(localStorage.getItem("carritoProductos")) || [];
 
-            const cantidadIngresada = row.querySelector(".numero");
-            const cantidad = parseInt
+        if (cantidad > 0 && cantidad <= element.stock) {
+          const productoSeleccionado = {
+            id: element.id,
+            producto: element.producto,
+            precio: element.precio,
+            cantidad: cantidad,
+          };
+          productosGuardados.push(productoSeleccionado);
+          carritoProducto.push(productoSeleccionado);
+          localStorage.setItem(
+            "carritoProductos",
+            JSON.stringify(productosGuardados)
+          );
 
-            if (cantidad > 0 && cantidad <= element.stock) {
-                const productoSeleccionado = {
-                    id: element.id,
-                    producto: element.producto,
-                    precio: element.precio,
-                    cantidad: cantidad
-                };
-                carritoProducto.push(productoSeleccionado);
-                actualizarCarrito();
-            }
-        });
-    });
+          actualizarCarrito();
+        }
+      });
+    }
+  });
 }
 
-
 function actualizarCarrito() {
-    // Limpiar contenido previo del carrito
-    carrito.innerHTML = "";
+  // Limpiar contenido previo del carrito
+  carro.innerHTML = "";
 
-    // Mostrar los productos en el carrito
-    carritoProducto.forEach(producto => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
+  // Mostrar los productos en el carrito
+  carritoProducto.forEach((producto) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
             <td>${producto.producto}</td>
             <td>${producto.precio}</td>
             <td>${producto.cantidad}</td>
             <td>${producto.precio * producto.cantidad}</td>
         `;
-        carrito.appendChild(row);
-    });
+    carro.appendChild(row);
+  });
 }
 lista.addEventListener("change", (event) => {
-    if (event.target.classList.contains("cantidad")) {
-        updateSubtotal(event.target);
-    }
+  if (event.target.classList.contains("cantidad")) {
+    updateSubtotal(event.target);
+  }
 });
-
-
-inputCantidad.addEventListener("change", () => {
+document.addEventListener("DOMContentLoaded", function () {
+  inputCantidad.addEventListener("change", () => {
     const mostrarSubTotal = document.getElementsByClassName("subTotal");
     mostrarSubTotal = "";
     const precioSubTotal = data.precio * inputCantidad;
     mostrarSubTotal.innerHTML = `
-        ${precioSubTotal};
-    `;
+          ${precioSubTotal};
+      `;
+  });
 });
